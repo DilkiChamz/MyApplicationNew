903b9ea76380bf6b2b1f76dd26308725aa2736fa
student <diltharu18@gmail.com>
2019-07-22 16:30:55 +0530

    first program commit

---
+++ app/src/main/java/com/example/student/myapplicationnew/MainActivityNew.java
@@ -0,0 +1,13 @@
+package com.example.student.myapplicationnew;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivityNew extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_new);
+    }
+}
